0b933417962e06ca5aff48f5d8a5624063cd8cc3
Barry Andersen <BarryA@BARRYA-W8-LP.nvisionit.co.za>
2015-07-06 18:19:45 +0200

    Added the solution, mobile azure service and the tests project

---
+++ Broders/Broders.Tests/OcrControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using brodersService.Controllers;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+
+namespace Broders.Tests
+{
+    [TestClass]
+    public class OcrControllerTests
+    {
+
+        //OCRController ocrController {get;set;}
+
+        //[TestInitialize]
+        //public void Setup
+        //{
+        //    ocrController = new OCRController(); 
+        //}
+
+
+        [TestMethod]
+        public void UploadFileToWebApi()
+        {
+            var ocrController = new OCRController();
+            //var pdfToUpload = File.Open(@"../../Standard OTP.pdf", FileMode.Open);
+            string baseurl = "http://localhost:50265/";
+
+
+            var imageFile = @"../../Standard OTP.pdf";
+            WebClient webClient = new WebClient();
+            byte[] rawResponse = webClient.UploadFile(string.Format("{0}/api/ocr/", baseurl), imageFile);
+            Console.WriteLine("Sever Response: {0}", System.Text.Encoding.ASCII.GetString(rawResponse)); // for debugging purposes
+            Console.WriteLine("File Upload was successful");
+
+
+        }
+    }
+}
---
+++ Broders/brodersService/Controllers/OCRController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace brodersService.Controllers
+{
+    public class OCRController : ApiController
+    {
+        // GET: api/OCR
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/OCR/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/OCR
+        public HttpResponseMessage Post()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            return result;
+        }
+
+        // PUT: api/OCR/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/OCR/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
